86501fbba2a7f28fa78c1a21ac9b101d007dfbf6
Nicholas Hazel <nicholas.hazel@gmail.com>
2020-11-29 12:42:52 -0800

    feat: Data binding with disabled

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
 
   ngOnInit(): void {
   }
