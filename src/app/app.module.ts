bbbaa0a6f400e747639710649891b70de4826478
Nicholas Hazel <nicholas.hazel@gmail.com>
2020-11-28 21:59:55 -0800

    feat: Added server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
+// Core
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
